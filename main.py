1ff690128e3e81bd3752873ffc7232e78710b03b
Susanne Kreuzer <a11911757@univie.ac.at>
2024-05-07 15:16:14 +0200

    Assessment 03

--- main.py
+++ main.py
@@ -1,14 +1,32 @@
 """
 Upper and Lower
 """
-# Provide your solution here
+my_string = input(">> Enter a phrase: ")
 
+def count_upper_lower(my_string: str) -> str:
+    for letter in my_string:
+        my_string += 1
+        if letter.isupper(): return letter
+        elif letter.islower(): return letter
+
+print(">> No. of Upper case characters:", my_string.isupper())
+print(">> No. of Lower case characters:", my_string.islower())
+
+count_upper_lower()
 """
 Check 33
 """
-# Provide your solution here
+
+my_list = int(input(">> Enter several numbers: "))
+def has_33(my_list) -> int:
+    for index in range(0, len(my_list)):
+        while my_list == [3, 3]: return True
+        if my_list != [3, 3]: return False
+
+has_33()
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print("Test your functions by calling them here. Use different parameter values to test them with different scenarios.")
+    print(count_upper_lower())
 
